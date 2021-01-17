0d6c1be3d1a2b5980a068e492ead44ca52667115
Vlad Cirstean <vcirstean@bitdefender.com>
2021-01-17 20:37:01 +0200

    initial commit

---
+++ web/index.js
@@ -0,0 +1,6 @@
+async function run() {
+    const abi = document.getElementById('abi').value;
+    const ctrAdr = document.getElementById('ctrAdr').value;
+    console.log(abi, ctrAdr);
+    // console.log(await web3.eth.defaultAccount);
+};
